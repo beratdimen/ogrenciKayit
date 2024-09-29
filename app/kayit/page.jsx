209bdf75a3d0a8ef47011a0e995edf42aa6472ee
TunahanOrak <164731717+TunahanOrak@users.noreply.github.com>
2024-09-29 13:37:01 +0300

    ücüncü sayfa bitti

--- app/kayit/page.jsx
+++ app/kayit/page.jsx
@@ -1,5 +1,7 @@
 "use client";
 
+import EvaluationForm from "../kayıt3/page";
+
 export default function Kayit() {
   return (
     <>
@@ -25,6 +27,7 @@ export default function Kayit() {
           <input type="text" />
         </label>
       </form>
+<EvaluationForm/>
     </>
   );
 }
