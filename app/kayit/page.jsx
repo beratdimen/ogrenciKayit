5f863338ca22ee67acd60a9e14f5327ac6c958fd
Berat Dimen <beratefedimen@hotmail.com>
2024-09-29 12:18:16 +0300

    first commit

---
+++ app/kayit/page.jsx
@@ -0,0 +1,30 @@
+"use client";
+
+export default function Kayit() {
+  return (
+    <>
+      <h1>Kayıt Sayfası</h1>
+      <form>
+        <label>
+          <input type="text" placeholder="isim"/>
+        </label>
+
+        <label>
+          <input type="text" placeholder="soy isim"/>
+        </label>
+
+        <label>
+          <input type="date" placeholder="doğum tarihi" />
+        </label>
+
+        <label>
+          <input type="number" placeholder="tc" />
+        </label>
+
+        <label>
+          <input type="text" />
+        </label>
+      </form>
+    </>
+  );
+}
