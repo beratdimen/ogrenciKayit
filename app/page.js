2a0928fafafa739ace9807bdd2c3f2a52aed3d9c
Berat Dimen <beratefedimen@hotmail.com>
2024-10-03 18:28:47 +0300

    3. sayfa step 3 e eşitse diye eklendi

--- app/page.js
+++ app/page.js
@@ -1,11 +1,9 @@
-import PageThree from "@/components/pagethree";
 import Kayit from "./kayit/page";
 
 export default function Home() {
   return (
     <div>
       <Kayit />
-      <PageThree />
     </div>
   );
 }
