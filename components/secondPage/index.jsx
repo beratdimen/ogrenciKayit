7a5f4623af608f4139abde96b20298042ce4e140
Berat Dimen <beratefedimen@hotmail.com>
2024-10-03 18:26:03 +0300

    1. sayfa da css eklenmesi yapıldı

--- components/secondPage/index.jsx
+++ components/secondPage/index.jsx
@@ -1,46 +1,26 @@
 export default function SecondPage({ errorsState }) {
   return (
     <>
-      <p>
-        <label htmlFor="gradeone">
-          <input
-            type="text"
-            name="gradeone"
-            id="gradeone"
-            placeholder="grade1"
-          />
-        </label>
-        <br />
-        {errorsState?.errors?.gradeone && (
-          <p>{errorsState?.errors?.gradeone}</p>
-        )}
-      </p>
-      <p>
-        <label htmlFor="gradetwo">
-          <input
-            type="text"
-            name="gradetwo"
-            id="gradetwo"
-            placeholder="grade2"
-          />
-        </label>
-        {errorsState?.errors?.gradetwo && (
-          <p>{errorsState?.errors?.gradetwo}</p>
-        )}
-      </p>
-      <p>
-        <label htmlFor="gradethere">
-          <input
-            type="number"
-            name="gradethere"
-            id="gradethere"
-            placeholder="telefon numaranız"
-          />
-        </label>
-        {errorsState?.errors?.gradethere && (
-          <p>{errorsState?.errors?.gradethere}</p>
-        )}
-      </p>
+      <label htmlFor="gradeone">
+        <input type="text" name="gradeone" id="gradeone" placeholder="grade1" />
+      </label>
+      <br />
+      {errorsState?.errors?.gradeone && <p>{errorsState?.errors?.gradeone}</p>}
+      <label htmlFor="gradetwo">
+        <input type="text" name="gradetwo" id="gradetwo" placeholder="grade2" />
+      </label>
+      {errorsState?.errors?.gradetwo && <p>{errorsState?.errors?.gradetwo}</p>}
+      <label htmlFor="gradethere">
+        <input
+          type="number"
+          name="gradethere"
+          id="gradethere"
+          placeholder="telefon numaranız"
+        />
+      </label>
+      {errorsState?.errors?.gradethere && (
+        <p>{errorsState?.errors?.gradethere}</p>
+      )}
     </>
   );
 }
